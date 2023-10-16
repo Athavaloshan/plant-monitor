343dcaef88f00965e831b2d776fb5ed4cdad0588
Athavaloshan Thirulingam <qbz6506@autuni.ac.nz>
2023-10-16 13:15:30 +1300

    custom graph with date picker

--- app/src/main/java/com/android/plantmonitor/activity/Temperature.java
+++ app/src/main/java/com/android/plantmonitor/activity/Temperature.java
@@ -1,6 +1,8 @@
 package com.android.plantmonitor.activity;
 
-import com.android.plantmonitor.fragment.DatePickerFragment;
+//import com.android.plantmonitor.fragment.DatePickerFragment;
+
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -30,26 +32,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.android.plantmonitor.databinding.ActivityTemperatureBinding;
+import java.lang.Math;
+import java.util.Calendar;
 
 import com.android.plantmonitor.R;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Temperature extends AppCompatActivity {
     private LineChart mChart;
-    private Button dashboard_btn, datePicker_start_btn, datePicker_end_btn;
+    private Button dashboard_btn, datePicker_start_btn, datePicker_end_btn, show_btn;
+    int startYear = 2020, endYear = 2023;
+    private int  getRandomNumber(){
+        int max = 35;
+        int min = 10;
+        int range = max - min + 1;
+        int rand = (int)(Math.random() * range) + min;
+
+        return rand;
+            // Output is different everytime this code is executed
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,41 +76,69 @@ public class Temperature extends AppCompatActivity {
             }
         });
 
+
         datePicker_start_btn = findViewById(R.id.datePicker_start);
         datePicker_end_btn = findViewById(R.id.datePicker_end);
 
         datePicker_start_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                DatePickerFragment newFragment = new DatePickerFragment();
-                newFragment.show(getSupportFragmentManager(), "datePicker");            }
+//                DatePickerFragment datePickerStart = new DatePickerFragment();
+//                datePickerStart.show(getSupportFragmentManager(), "datePicker start");
+//                startYear = datePickerStart.mYear;
+                openDialog(true);
+            }
         });
 
         datePicker_end_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                DatePickerFragment newFragment = new DatePickerFragment();
-                newFragment.show(getSupportFragmentManager(), "datePicker");            }
+//                DatePickerFragment datePickerEnd = new DatePickerFragment();
+//                datePickerEnd.show(getSupportFragmentManager(), "datePicker end");
+//                endYear = datePickerEnd.mYear;
+                openDialog(false);
+            }
+
+        });
+
+        show_btn = findViewById(R.id.graph_show_btn);
+        show_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                renderData();}
         });
     }
+    private void openDialog(boolean isStart){
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
 
+        // Create a new instance of DatePickerDialog and return it.
+        DatePickerDialog dialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                if (isStart) {
+                    startYear = year;
+                }
+                else {
+                    endYear = year;
+                }
+            }
+        }, year, month, day);
+        dialog.show();
+    }
     public void renderData() {
-        LimitLine llXAxis = new LimitLine(10f, "Index 10");
-        llXAxis.setLineWidth(4f);
-        llXAxis.enableDashedLine(10f, 10f, 0f);
-        llXAxis.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
-        llXAxis.setTextSize(10f);
-
         XAxis xAxis = mChart.getXAxis();
         xAxis.enableGridDashedLine(10f, 10f, 0f);
-        xAxis.setAxisMaximum(10f);
-        xAxis.setAxisMinimum(0f);
+        xAxis.setAxisMaximum(endYear);
+        xAxis.setAxisMinimum(startYear);
         xAxis.setDrawLimitLinesBehindData(true);
 
         YAxis leftAxis = mChart.getAxisLeft();
         leftAxis.removeAllLimitLines();
-        leftAxis.setAxisMaximum(350f);
-        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(35f);
+        leftAxis.setAxisMinimum(10f);
         leftAxis.enableGridDashedLine(10f, 10f, 0f);
         leftAxis.setDrawZeroLine(false);
         leftAxis.setDrawLimitLinesBehindData(false);
@@ -115,22 +150,20 @@ public class Temperature extends AppCompatActivity {
     private void setData() {
 
         ArrayList<Entry> values = new ArrayList<>();
-        values.add(new Entry(1, 50));
-        values.add(new Entry(2, 100));
-        values.add(new Entry(3, 80));
-        values.add(new Entry(4, 120));
-        values.add(new Entry(5, 110));
-        values.add(new Entry(7, 150));
-        values.add(new Entry(8, 250));
-        values.add(new Entry(9, 190));
+
+        for (int i = startYear; i <= endYear; ++i) {
+            values.add(new Entry(i, getRandomNumber()));
+        }
 
         LineDataSet set1;
+
         if (mChart.getData() != null &&
                 mChart.getData().getDataSetCount() > 0) {
             set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
             set1.setValues(values);
             mChart.getData().notifyDataChanged();
             mChart.notifyDataSetChanged();
+            mChart.invalidate();
         } else {
             set1 = new LineDataSet(values, "Sample Data");
             set1.setDrawIcons(false);
